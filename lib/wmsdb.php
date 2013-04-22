4db179343c8d2e79d7847874cfc63e3ec7e88bd8
Georg Lerm <glerm@gltech.co.za>
2013-04-22 14:20:40 +0300

    Update wmsdb.php
    Added some initial code from WMS side

--- lib/wmsdb.php
+++ lib/wmsdb.php
@@ -1,1 +1,14 @@
 //wrapper for db acccess for WMS_GUI
+
+<?php
+class WMS {
+    private $_db;
+
+    public function getDb ()
+      {
+        if (!$this->_db) {
+        require_once('lib/config.php');
+        $this->_db = new PDO('mysql:dbname=' . $DBNAME . ';host=' . $DBHOST, $DBUSER, $DBPASS);
+        }
+      return $this->_db;}
+}
